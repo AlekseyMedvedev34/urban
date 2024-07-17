6a7c015c057099a93ca08e8e8ae052924d51e49a
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-07-17 14:55:11 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+first_name = 'Alex,'
+number_of_hours = 12
+course_name = 'Python,'
+time_for_one = 1.5
+Time_for_one_task = time_for_one / number_of_hours
+
+print(first_name, course_name, number_of_hours, 'заданий,', time_for_one, 'времени затрачено на уроки,', Time_for_one_task, 'среднее время выполния')
