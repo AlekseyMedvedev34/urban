c628be796e380bc0b0aa8e7a785ece8253649cdc
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-07-18 19:30:46 +0300

    Add files via upload
    Поправил замечание - Использовать методы строк нужна для введенной пользователем строки input()

---
+++ homework4.1.py
@@ -0,0 +1,15 @@
+my_string = input('В ведите название: ')
+print(input(my_string))
+print('В ведите название:'.upper())
+
+print('В ВЕДИТЕ НАЗВАНИЕ:'.lower())
+
+print('В ведите название'.replace(' ', ''))
+
+my_string = ('В ведите название: ')
+print(my_string[0])
+my_string = ('В ведите название: ')
+print(my_string[-3])
+
+
+
