96101eca9a7f87ad16ada3ba4dda8a930107adbc
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-07-18 15:22:44 +0300

    Add files via upload

---
+++ homework3.1.py
@@ -0,0 +1,10 @@
+name = 'Алексей'
+print ('Name: '+ name)
+age = 'Age:  45'
+print(age)
+age=45
+new_age = (age)+1
+new_age = 'New age: 46'
+print(new_age)
+is_student = ('is_student ' + str(True))
+print (is_student)
