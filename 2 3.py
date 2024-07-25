db891ae88c375c141132b23f4939f195da6598a5
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-07-25 08:26:23 +0300

    Add files via upload

---
+++ 2 3.py
@@ -0,0 +1,12 @@
+# while 10 > 0:
+#     my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+#     len = my_list
+#     print( len )
+#     continue
+# if  0 or < 0:
+#     break
+
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+
+print( <0 )
