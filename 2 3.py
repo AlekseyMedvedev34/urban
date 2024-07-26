7d5fdba0aef0d70b014dff3469c2f3c6c6d9f414
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-07-26 19:41:00 +0300

    Update 2 3.py

--- 2 3.py
+++ 2 3.py
@@ -1,12 +1,13 @@
-# while 10 > 0:
-#     my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-#     len = my_list
-#     print( len )
-#     continue
-# if  0 or < 0:
-#     break
-
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
 index = 0
 
-print( <0 )
+while index < len(my_list):
+
+    if my_list[index] < 0:
+        break
+
+    if my_list[index] > 0:
+        print(my_list[index])
+
+    index += 1
