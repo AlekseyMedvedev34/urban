73a8acf3527d5f2f8647e9ff1ad5bdd4c8b139ca
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-08-15 11:13:59 +0300

    Update module_3_1.py
    Поправил код, теперь есть значение True и False

--- module_3_1.py
+++ module_3_1.py
@@ -1,26 +1,28 @@
-
-
-
-def count_calls():
+def count_calls(call):
     global calls
-    calls=+1
-
+    calls += call
+    return calls
 
 
 def string_info(string):
-    count_calls()
-    return len(string), string.upper(), string.lower()
+    a=[]
+    a.append(len(string))
+    a.append(string.upper())
+    a.append(string.lower())
+    count_calls( 1 )
+    return a
 
 
-
-def is_contains(string, list_to_search ):
-    count_calls()
-    return string in list_to_search
+def is_contains(string_info, is_contains ):
+    string_info.lower()
+    new_is_contains = []
+    count_calls( 1 )
+    for i in is_contains:
+        new_is_contains.append( i.lower() )
+    return (string_info.lower() in new_is_contains)
 
 
 calls = 0
-
-
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
