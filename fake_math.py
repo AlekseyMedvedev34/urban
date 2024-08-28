bbe7dbc7cf13449554da0b2a1ef33b8929933541
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-08-28 20:49:57 +0300

    4.1

---
+++ fake_math.py
@@ -0,0 +1,14 @@
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+
+    else:
+        return first/second
+# first = int( input( 'Ввидите число 1: ' ) )
+# second = int( input( 'Введите число 2: ' ) )
+# print( 'Ошибка' )
+#
+# print(divide(49, 7))
+# print(divide(15, 0))
