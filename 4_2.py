ad455359771af6dbc9c702d0bd8e266959b2b751
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-09-02 13:34:26 +0300

    Update 4_2.py
    Внес изменения  где необходимо вызвать функцию inner_function() внутри test_function().

--- 4_2.py
+++ 4_2.py
@@ -2,11 +2,8 @@
 def test_function():
     print('Я в области видимости test_function')
     def inner_function():
-        print()
+        print(9956)
+    inner_function()
 
 test_function()
-inner_function()
-
-
-# inner_function()
 
