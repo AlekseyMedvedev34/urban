4abab1024297033a309289f0bb7458d6c57da54f
AlekseyMedvedev34 <92936159+AlekseyMedvedev34@users.noreply.github.com>
2024-10-11 16:11:42 +0300

    Update module_3_3.py
    Поправил код,  используя * для списка и ** для словаря

--- module_3_3.py
+++ module_3_3.py
@@ -1,16 +1,16 @@
-def print_params(a=1, b='строка', c=True):
+def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
 
-print_params()
-print_params(b=True, c={'a': 2})
-print_params(b=25)
-print_params(c=[1, 2, 3])
+# print_params()
+# print_params(b=True, c={'a': 2})
+print_params(b = 25)
+print_params(c = [1, 2, 3])
 
 values_list = (1, 2, 3)
-print(values_list)
+print_params(*values_list)
 
 values_dict = {'a': 1, 'b': 2, 'c': 3}
-print(values_dict)  # Исправлено: убрана распаковка словаря
+print_params(**values_dict)  # Исправлено: убрана распаковка словаря
 
 values_list_2 = [54.32, 'Строка']
-print(values_list_2)
+print_params(*values_list_2)
